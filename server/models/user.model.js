a285dceb4391978170973eec6460bb35d1d923de
Antet <anteexpress@hotmail.com>
2021-01-10 20:45:36 +0100

    add routing

---
+++ server/models/user.model.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) =>
+  sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      phone: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      updatedAt: false,
+      createdAt: false,
+      underscored: true,
+    }
+  );
