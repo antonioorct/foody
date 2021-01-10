12f8c0875b11716d893f14a396e7134af891d184
Antet <anteexpress@hotmail.com>
2021-01-10 22:01:51 +0100

    add routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,31 @@
-function App() {
-  return <div></div>;
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import ProtectedRoute from "../components/protectedRoute";
+import { UserContext } from "../contexts/UserContext";
+import HeaderBar from "./headerBar";
+import Login from "./login";
+import Register from "./register";
 
-export default App;
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser({ name: "Ante" });
+  }, []);
+
+  return user ? (
+    <UserContext.Provider value={[user, setUser]}>
+      <HeaderBar />
+      <Container>
+        <Switch>
+          <Route path="/login" component={Login} exact></Route>
+          <Route path="/register" component={Register} exact></Route>
+        </Switch>
+      </Container>
+    </UserContext.Provider>
+  ) : (
+    <div />
+  );
+}
