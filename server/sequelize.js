9c07c5f4b586253de9924b17c8ec09df43c146ff
Antet <anteexpress@hotmail.com>
2021-01-11 09:52:02 +0100

    add error checking to sequelize

--- server/sequelize.js
+++ server/sequelize.js
@@ -4,13 +4,14 @@ const sequelize = new Sequelize(
   `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:3306/${process.env.DB_DB}`
 );
 
-try {
-  console.log("Connected to database.");
-} catch (e) {
-  console.log("Unable to connect to the database:");
-  console.log(e.message);
-  process.exit(1);
-}
+sequelize
+  .authenticate()
+  .then(console.log("Connected to database."))
+  .catch((e) => {
+    console.log("Unable to connect to the database:");
+    console.log(e.message);
+    process.exit(1);
+  });
 
 const models = [
   require("./models/user.model"),
