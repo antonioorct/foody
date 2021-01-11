f508d361a3ea83527fc213ae08015d5c96d05baf
Antet <anteexpress@hotmail.com>
2021-01-11 10:36:44 +0100

    fix userLocations route bug

--- server/routes/userLocations.js
+++ server/routes/userLocations.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { models } = require("../sequelize");
 
 router.get("/:userId", async function (req, res) {
-  const locations = await models.location.findAll({
+  const userLocations = await models.userLocation.findAll({
     where: { userId: req.params.userId },
   });
 
-  res.status(200).send(locations);
+  res.status(200).send(userLocations);
 });
 
 module.exports = router;
