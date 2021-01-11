8b8f5cff49f346227fc977ed913ea2c78fec703d
Antet <anteexpress@hotmail.com>
2021-01-11 09:44:25 +0100

    fix login form submit

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -5,13 +5,16 @@ import useForm from "../utils/useForm";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import { useHistory } from "react-router-dom";
 
-export default function Register() {
+export default function Login() {
   const [form, handleChange] = useForm();
+  const history = useHistory();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    login(form);
+    await login(form);
+    history.push("/");
   };
 
   return (
@@ -19,12 +22,14 @@ export default function Register() {
       <FormControl
         name="username"
         value={form.username}
+        onChange={handleChange}
         placeholder="Username"
       />
       <FormControl
         type="password"
         name="password"
         value={form.password}
+        onChange={handleChange}
         placeholder="Password"
       />
       <Button type="submit">Submit</Button>
