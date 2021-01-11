6851f9173225a57d2394039fca45045067114d06
Antet <anteexpress@hotmail.com>
2021-01-11 09:49:54 +0100

    remove unnecessary code

--- client/src/utils/useForm.js
+++ client/src/utils/useForm.js
@@ -3,8 +3,9 @@ import { useState } from "react";
 export default function useForm() {
   const [state, setState] = useState({});
 
-  const handleChange = ({ target }) =>
-    setState((prevState) => ({ ...prevState, [target.name]: target.value }));
+  const handleChange = ({ target }) => {
+    setState({ ...state, [target.name]: target.value });
+  };
 
   return [state, handleChange];
 }
