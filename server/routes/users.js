7579cfafb252e0d1b295870e878a6c5cb748004c
Antet <anteexpress@hotmail.com>
2021-01-11 09:52:32 +0100

    add GET route to users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { models } = require("../sequelize");
+const bcrypt = require("bcrypt");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res) {
+  res.send("respond with a resource");
+});
+
+router.post("/", async function (req, res) {
+  try {
+    req.body["password"] = await bcrypt.hash(req.body["password"], 10);
+    await models.user.create(req.body);
+
+    return res.status(201).send();
+  } catch (e) {
+    return res.status(400).send(e.message);
+  }
 });
 
 module.exports = router;
