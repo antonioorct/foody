5a95a07cdd92056d28c890b76ea1ab4c14125a14
Antet <anteexpress@hotmail.com>
2021-01-13 08:40:48 +0100

    fix orderService

--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -2,13 +2,28 @@ import http from "./httpService";
 
 const apiEndPoint = "http://localhost:3001/api/orders";
 
-export async function makeOrder(meals, userId, restaurantId, userLocationId) {
-  const newOrder = await http.post(apiEndPoint, {
+export async function getAllOrdersForUserId(userId) {
+  const { data } = await http.get(apiEndPoint + "/" + userId);
+
+  return data;
+}
+
+export async function makeOrder(
+  meals,
+  userId,
+  restaurantId,
+  userLocationId,
+  price
+) {
+  const { data } = await http.post(apiEndPoint, {
     meals,
     userId,
     restaurantId,
     userLocationId,
+    price,
   });
+}
 
-  console.log(newOrder);
+export async function deleteOrder(orderId) {
+  await http.delete(apiEndPoint + "/" + orderId);
 }
