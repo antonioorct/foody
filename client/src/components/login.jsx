478498f3ea0b6f637081158caeb4c85dcc7c0d5f
Antet <anteexpress@hotmail.com>
2021-01-13 15:00:40 +0100

    add error handling and validation to login

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -5,20 +5,27 @@ import useForm from "../utils/useForm";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import Feedback from "react-bootstrap/Feedback";
 import { Link, useHistory } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 export default function Login() {
   const [user, setUser] = useContext(UserContext);
-  const [form, handleChange] = useForm();
   const history = useHistory();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const doSubmit = async (e) => {
     const loggedInUser = await login(form);
     setUser(loggedInUser);
     history.push("/");
   };
+  const validation = {
+    username: { required: true, minLength: 4 },
+    password: { required: true },
+  };
+  const [form, errors, handleChange, handleSubmit] = useForm(
+    doSubmit,
+    validation
+  );
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -27,14 +34,19 @@ export default function Login() {
         value={form.username}
         onChange={handleChange}
         placeholder="Username"
+        isInvalid={errors.username}
       />
+      <Feedback type="invalid">{errors.username}</Feedback>
+
       <FormControl
         type="password"
         name="password"
         value={form.password}
         onChange={handleChange}
         placeholder="Password"
+        isInvalid={errors.password}
       />
+      <Feedback type="invalid">{errors.password}</Feedback>
 
       <div className="d-flex">
         <Button type="submit">Login</Button>
